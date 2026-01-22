2e7a97e29b370dcf472b79be7977fa05dde80e8c
Felipe <felipeszu.dev@gmail.com>
2026-01-22 12:46:03 -0300

    feat: implement GridBloc to manage grid state, interactions, and highlighting, and register it for dependency injection.

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -12,13 +12,17 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
+import '../../domain/repositories/grid_repository.dart' as _i213;
+import '../../presentation/bloc/grid_bloc.dart' as _i61;
+
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
   _i174.GetIt init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    gh.factory<_i61.GridBloc>(() => _i61.GridBloc(gh<_i213.GridRepository>()));
     return this;
   }
 }
