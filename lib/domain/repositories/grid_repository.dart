d5b2bdbafc518edf78aca36840221ea668f838f3
Felipe <felipeszu.dev@gmail.com>
2026-01-22 12:43:50 -0300

    feat: Add Grid BLoC for managing grid state and cell interactions.

---
+++ lib/domain/repositories/grid_repository.dart
@@ -0,0 +1,8 @@
+import 'dart:math';
+import 'package:fibo_grid/domain/entities/grid_cell.dart';
+
+abstract class GridRepository {
+  Future<Map<String, GridCell>> loadGrid();
+  Future<void> saveGrid(Map<String, GridCell> grid);
+  Future<void> clearGrid();
+}
